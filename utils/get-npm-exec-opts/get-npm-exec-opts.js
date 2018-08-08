653f239aedfbb38ca87aaf60d7c120f25cf9acd1
Daniel Stockman <daniel.stockman@gmail.com>
2018-08-08 11:58:05 -0700

    feat(get-npm-exec-opts): Accept otp, set env var if present

--- utils/get-npm-exec-opts/get-npm-exec-opts.js
+++ utils/get-npm-exec-opts/get-npm-exec-opts.js
@@ -4,7 +4,7 @@ const log = require("libnpm/log");
 
 module.exports = getExecOpts;
 
-function getExecOpts(pkg, registry) {
+function getExecOpts(pkg, registry, otp) {
   // execa automatically extends process.env
   const env = {};
 
@@ -12,7 +12,11 @@ function getExecOpts(pkg, registry) {
     env.npm_config_registry = registry;
   }
 
-  log.silly("getExecOpts", pkg.location, registry);
+  if (otp) {
+    env.npm_config_otp = otp;
+  }
+
+  log.silly("getExecOpts", pkg.location, registry, otp);
   return {
     cwd: pkg.location,
     env,
